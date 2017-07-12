e591c9a5ce7cd52b3b055a98ebe93dc134cc82c7
Shakthi <shakthivvbhss@gmail.com>
2017-07-12 14:55:11 +0530

    Adding app.module file

--- GoodMovieApp/src/app/app.component/app.module.ts
+++ GoodMovieApp/src/app/app.component/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdTabsModule, MdGridListModule, MdSlideToggleModule, MdAutocompleteModule, MdInputModule, MdDialogModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MovieDetailsComponent } from '../movie-details.component/movie-details.component';
+import { DashboardComponent } from '../dashboard.component/dashboard.component';
+import { MovieListComponent } from '../movie-list.component/movie-list.component';
+import { MovieSearchComponent } from '../movie-search.component/movie-search.component';
+import { MoviesComponent } from '../movies.components/movies.components';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MoviesComponent,
+    MovieDetailsComponent,
+    MovieListComponent,
+    MovieSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    MdButtonModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdMenuModule,
+    MdIconModule,
+    MdTabsModule,
+    MdGridListModule,
+    MdSlideToggleModule,
+    MdAutocompleteModule,
+    MdInputModule,
+    MdDialogModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [MovieDetailsComponent],
+  providers: [MdDialogModule],
+  bootstrap: [AppComponent ]
 })
+
+
 export class AppModule { }
