8974405ac064a8416d20f02deaff50205d2a7566
Shakthi <shakthivvbhss@gmail.com>
2017-07-20 17:13:51 +0530

    creating movie-detail.component.js file to store and fetch from Mongodb

--- GoodMovieApp/src/app/app.component/app.module.ts
+++ GoodMovieApp/src/app/app.component/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from "@angular/router";
+import { RouterModule,Routes } from "@angular/router";
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
---
+++ GoodMovieApp/src/app/movie-details.component/movie-details.component.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
