c988496ff7354a2f891270ac0b6ca982726a529c
Shakthi <shakthivvbhss@gmail.com>
2017-07-21 12:30:32 +0530

    working on movie-details.component.js to store and fetch the data from Mongo DB

--- GoodMovieApp/src/app/movie-details.component/movie-details.component.js
+++ GoodMovieApp/src/app/movie-details.component/movie-details.component.js
@@ -2,3 +2,42 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
+
+var url = 'mongodb://localhost:27017';
+
+router.get('/',function(req,res,next){
+    res.render('../app.component/app.component');
+});
+
+router.post('/',function(req,res,next){
+    var details = {
+        Rating: req.body.inputRate,
+        Review: req.body.inputReview
+    }
+});
+
+router.get('/',function(req,res,next){
+    var resultArray = [];
+    mongo.connect(url,function(err,db){
+        assert.equal(null,err);
+        var current = db.collection('user-data').find();
+        current.forEach(function(doc,err){
+            assert.equal(null,err);
+            resultArray.push(doc);
+        },function(){
+            db.close();
+            res.render('../app.component/app.component',{index:resultArray});
+        });
+    });
+});
+
+mongo.connect(url,function(err,db){
+    assert.equal(null,err);
+    db.collection('user-data').insertOne(item, function(err, result){
+        assert.equal(null,error);
+        console.log('Item Inserted');
+        db.close();
+    });
+});
+
+module.exports = router;
